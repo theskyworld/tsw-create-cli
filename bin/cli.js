37c4b4cbf88c33d3c64829085d3324c76fd7eb48
theskyworld <2456450687@qq.com>
2024-01-14 10:20:48 +0800

    feat: add name, version config to commander

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,9 @@
 #! /usr/bin/env node
 
-console.log("hello");
+import { program } from "commander";
+import require from "./utils/useRequire.js";
+
+// require的路径值应当相对于useRequire.js文件的路径进行填写
+program.name("tsw").version(`v${require("../../package.json").version}`); // 配置动态版本号
+
+program.parse(process.argv);
